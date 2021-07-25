6d85013a1cb301daa6c8b9eff70a2da0777216ed
brychanmd <bmorrisdafydd@gmail.com>
2021-07-25 15:10:01 +0100

    task tracker vue, started

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button :style="{ background: colour }" class="btn" @click="onClick()" >{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: {
+            type: String
+        },
+        colour: {
+            type: String
+        }
+    },
+    methods: {
+        onClick() {
+            console.log('click')
+        },
+    }
+}
+</script>
